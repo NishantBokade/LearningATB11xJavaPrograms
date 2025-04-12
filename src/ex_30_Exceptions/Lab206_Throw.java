3d7ef480b7780a04d3c0ba4261f97abed0939695
nisha <nishantbokade@gmail.com>
2025-04-12 21:52:12 +0530

    10th april- push code

---
+++ src/ex_30_Exceptions/Lab206_Throw.java
@@ -0,0 +1,17 @@
+package ex_30_Exceptions;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+public class Lab206_Throw {
+    public static void main(String[] args) {
+
+    }
+        static void divideByZero (int a){
+            // check some
+            if (a == 0) {
+                throw new ArithmeticException();
+            }
+        }
+    }
+
