808d9f3d8a2bda9fea70f42f5cdef700a74ebff3
nisha <nishantbokade@gmail.com>
2025-04-15 23:45:47 +0530

    14th april- push code

---
+++ src/ex_32_Collection_framework/List/Lab215_Linkedlist.java
@@ -0,0 +1,20 @@
+package ex_32_Collection_framework.List;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Lab215_Linkedlist {
+    public static void main(String[] args) {
+
+        List list = new LinkedList();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+
+
+
+    }
+
+
+
+}
